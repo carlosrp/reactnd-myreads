f47ed8fc6bc1957b620a8a357bc89fbebf488431
Carlos Rodriguez <carlos@rodriguez.eu.com>
2017-08-09 00:06:49 +1000

    Created component ShelfBooks and modified App.js to get books via remote BooksAPI

---
+++ src/ShelfBooks.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class ShelfBooks extends Component {
+
+  static propTypes = {
+    shelfName: PropTypes.string.isRequired,
+    shelfTitle: PropTypes.string.isRequired,
+    shelfBooks: PropTypes.array.isRequired
+  }
+
+  render() {
+    const { shelfTitle, shelfBooks } = this.props
+
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{shelfTitle}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {shelfBooks.map((book) => (
+              <li key={book.id}>
+                <div className="book">
+                  <div className="book-top">
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                    <div className="book-shelf-changer">
+                      <select>
+                        <option value="none" disabled>Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div className="book-title">{book.title}</div>
+                  <div className="book-authors">{book.authors}</div>
+                </div>
+              </li>
+            ))}
+          </ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ShelfBooks
